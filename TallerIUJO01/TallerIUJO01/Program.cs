2dcfb24f5a56b1e7e9070d232faec87f817aeb3d
Siraxx8 <jsiralucena73@gmail.com>
2026-04-17 10:55:53 -0400

    creando proyecto

---
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: usuario
+ * Fecha: 17/4/2026
+ * Hora: 10:53 a. m.
+ * 
+ * Puto el que lo lea
+ */
+using System;
+
+namespace TallerIUJO01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
