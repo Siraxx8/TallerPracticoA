6ee7e928bdb6d3df69feff5e3760e5ded23b587e
Siraxx8 <jsiralucena73@gmail.com>
2026-04-17 11:58:48 -0400

    revision error archivo

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -19,13 +19,31 @@ namespace TallerIUJO01
 			
 			string[] partes = registrolimpio.Split(';');
 			string id = partes[0].Trim();
-			string idnuevo = id.Replace('_',' ');
 			string nombre = partes[1].Trim();
 			string tarea = partes[2].Trim();
 			string nota = partes[3].Trim();
 			
-			Console.WriteLine(string.Format("El ID llamado: {0} del usuario: {1} con la tarea: {2} tiene nota de: {3}", idnuevo ,nombre, tarea, nota));
+			Console.WriteLine(string.Format("El ID llamado {0} del usuario {1} con la tarea {2} tiene nota de {3}", id, tarea, nombre, nota));
+
+			//flujo en archivos			
+			string rutaraiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DATOSIUJO");
+			string archivotexto = Path.Combine(rutaraiz, "Notas.txt");
+			Console.WriteLine(archivotexto);
 			
+			
+			if (Directory.Exists(rutaraiz)) {
+				Directory.CreateDirectory(rutaraiz);
+				Console.WriteLine("directorio creado correctamente");
+				
+			}
+			string archivotexto = Path.Combine(rutaraiz, "notas.txt");
+			using (StreamWriter sw = new StreamWriter(archivotexto,true))
+			{
+				sw.WriteLine(string.Format("ID : {0}, nota {1} Fecha:{yyyy-MM-dd HH:mm}", id, nota, DateTime.now));
+			
+			
+			}
+	
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
