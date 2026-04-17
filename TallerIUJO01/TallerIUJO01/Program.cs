1b9034c27cc232c3dd27fd4f0faeaf72f735a90c
Siraxx8 <jsiralucena73@gmail.com>
2026-04-17 11:29:59 -0400

    agregando manipulacion de texto

--- TallerIUJO01/TallerIUJO01/Program.cs
+++ TallerIUJO01/TallerIUJO01/Program.cs
@@ -1,12 +1,6 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: usuario
- * Fecha: 17/4/2026
- * Hora: 10:53 a. m.
- * 
- * Puto el que lo lea
- */
-using System;
+﻿using System;
+using System.IO;
+using System.Text;
 
 namespace TallerIUJO01
 {
@@ -14,9 +8,23 @@ namespace TallerIUJO01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("***Taller 01***");
 			
-			// TODO: Implement Functionality Here
+			//1. el dato del usuario
+			string registroUsuario = "		ID_777;		EVALUACION;		Jose Sira;		95" ;
+			Console.WriteLine(registroUsuario);
+			
+			string registrolimpio = registroUsuario.Trim().ToUpper();
+			Console.WriteLine(registrolimpio);
+			
+			string[] partes = registrolimpio.Split(';');
+			string id = partes[0].Trim();
+			string idnuevo = id.Replace('_',' ');
+			string nombre = partes[1].Trim();
+			string tarea = partes[2].Trim();
+			string nota = partes[3].Trim();
+			
+			Console.WriteLine(string.Format("El ID llamado: {0} del usuario: {1} con la tarea: {2} tiene nota de: {3}", idnuevo ,nombre, tarea, nota));
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
